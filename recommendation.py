6ae5d2995ffb664bdf2fcb66a1de54d4f9c53c63
Detective2016 <31264714+Detective2016@users.noreply.github.com>
2017-11-13 15:41:31 -0500

    setup backend connection point

---
+++ recommendation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# placeholder for actual recommendation logic
+def provide_recommendation(dict):
+    for key, value in dict.items():
+        print("key: " + key + " value: " + value)
