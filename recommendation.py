e190358dbf3dd49c7ea184afdf7fa883765bcffe
AlexKH22 <alexkh2249@gmail.com>
2017-11-15 14:24:59 -0500

    Gitignore

--- recommendation.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-# placeholder for actual recommendation logic
-def provide_recommendation(dict):
-    for key, value in dict.items():
-        print("key: " + key + " value: " + value)
