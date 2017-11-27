04751ac91b2b03973c50a588f4eb8e95c5b83996
SuMei <yuxue2017@gmail.com>
2017-11-27 00:42:39 -0500

    better format email

--- Project/email_generator.py
+++ Project/email_generator.py
@@ -1,9 +1,15 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text      import MIMEText
+from email.mime.image     import MIMEImage
 
 def sendemail(recommendation, recommendation_details, from_addr, to_addr, subject, message,
               login, password, smtpserver='smtp.gmail.com:587'):
 
-    message = "We recommend the following based on your location, driving style, hours, age, parking, and usage:\n"
+    message = '<img src="cid:image1"><br>' \
+              '<b>We recommend the following based on your ' \
+              '<i>location, driving style, hours, age, parking, ' \
+              'and usage</i>:</b><br>'
     if (recommendation['Keyloss']):
         message += " Keyloss "
     if (recommendation['Paint']):
@@ -12,21 +18,26 @@ def sendemail(recommendation, recommendation_details, from_addr, to_addr, subjec
         message += " Tires "
     if (recommendation['Windshield']):
         message += " Windshield "
-    message += "\n\n With the following recommendation details:\n"
+    message += '<br><br><b>With the following recommendation details:</b><br>'
     message += recommendation_details
-    msg = "\r\n".join([
-        "From: " + from_addr,
-        "To: " + to_addr,
-        "Subject: " + subject,
-        "",
-        message
-    ])
 
-    print(recommendation)
-    print(recommendation_details)
+    msgRoot = MIMEMultipart('related')
+    msgRoot["Subject"] = "BMWCare Recommendations"
+    msgAlternative = MIMEMultipart('alternative')
+    msgRoot.attach(msgAlternative)
+
+    msgText = MIMEText(message, 'html')
+    msgAlternative.attach(msgText)
+
+    fp = open('images/CT_logo1.png', 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+
+    msgImage.add_header('Content-ID', '<image1>')
+    msgRoot.attach(msgImage)
 
     server = smtplib.SMTP(smtpserver)
     server.starttls()
     server.login(login,password)
-    problems = server.sendmail(from_addr, to_addr, msg)
+    server.sendmail(from_addr, to_addr, msgRoot.as_string())
     server.quit()
